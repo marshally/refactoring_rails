f83f3309df725261166e6327d6a2066ea6723407
Marshall Yount <marshall@codeclimate.com>
2014-12-15 21:46:36 -0600

    rename @signup_form @signup

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,25 +1,25 @@
 class SignupsController < ApplicationController
   # GET /users/new
   def new
-    @signup_form = Signup.new
-    @user = @signup_form.user
+    @signup = Signup.new
+    @user = @signup.user
   end
 
 
   # POST /users
   # POST /users.json
   def create
-    @signup_form = Signup.new(params[:user])
-    @account = @signup_form.account
-    @user = @signup_form.user
+    @signup = Signup.new(params[:user])
+    @account = @signup.account
+    @user = @signup.user
 
     respond_to do |format|
-      if @signup_form.save
+      if @signup.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @signup_form.errors, status: :unprocessable_entity }
+        format.json { render json: @signup.errors, status: :unprocessable_entity }
       end
     end
   end
--- spec/views/signups/new.html.erb_spec.rb
+++ spec/views/signups/new.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "signups/new", :type => :view do
   before(:each) do
-    assign(:signup_form, Signup.new(
+    assign(:signup, Signup.new(
       :login => "MyString",
       :email => "MyString"
     ))
