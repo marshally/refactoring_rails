e1f78249e5d2469ca3db522911f30b1e73ea4581
Marshall Yount <marshall@codeclimate.com>
2014-12-16 15:31:35 -0600

    eliminate try with NullObject pattern

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,18 @@ class Account < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  class NullAccount
+    def name
+      ""
+    end
+  end
+
+  def self.for_user(user)
+    if user.account_id
+      Account.find(user.account_id)
+    else
+      NullAccount.new
+    end
+  end
 end
