1a380d788de37e5fba1ebd4e36eb65cf6c215ff5
Marshall Yount <marshall@codeclimate.com>
2014-12-15 19:45:06 -0600

    rails generate model Account name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215013920) do
+ActiveRecord::Schema.define(version: 20141216012629) do
+
+  create_table "accounts", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "login"
